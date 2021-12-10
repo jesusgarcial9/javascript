3b74e05f1a4a93735bc025e872a101ed8f11a15d
jesusgarcial9 <92926008+jesusgarcial9@users.noreply.github.com>
2021-12-10 13:52:19 +0100

    Add files via upload

---
+++ tarefa3_mates/escripito.js
@@ -0,0 +1,30 @@
+/* 
+	Exemplo ficheiro (a completar) en JavaScript
+	Nome do ficheiro: escripito.js
+	 
+*/
+
+/*VARIABLES*/
+var numero1;
+var numero2;
+var numero3;
+var numero4;
+numero1=1500.195;
+numero2=2000.525;
+numero3=2500.255;
+numero4=1000.144;
+
+/*RAÍZ CUADRADA D UNA VARIABLE NUMERICA*/
+alert(Math.sqrt(numero1));
+
+/*CUADRADO DE UNA VARIABLE*/
+alert(Math.pow(numero3,2));
+
+/*NUMEROPI*/
+alert(Math.round(Math.PI));
+
+/*Funcion para que devuelva la menor de las variables*/
+alert(Math.min(numero3,numero1,numero4,numero2));
+
+/*Una función que genera un número aleatorio entre cero y tres.*/
+alert(Math.random(0,3))
